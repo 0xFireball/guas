636c12abac0f961d43be571a134a7d40728abe0f
0xFireball <0xFireball@outlook.com>
2018-10-17 23:05:25 -0700

    add some basic char drawing functions

---
+++ source/graphics/termfont.d
@@ -0,0 +1,9 @@
+module guas.graphics.termfont;
+
+import raylib;
+
+struct TermFont {
+    Texture2D texture;
+    int gridSize;
+    int charSize;
+}
--- source/render.d
+++ source/render.d
@@ -3,27 +3,50 @@ module guas.render;
 import std.stdio;
 import std.string : fromStringz;
 import raylib;
+import raymath;
 
 import guas.game.world;
+import guas.graphics.termfont;
 import guas.resources;
 
 class Renderer {
     World _world;
     Color _clearColor;
-    Texture2D _font;
+    TermFont _font;
 
     this(World world) {
         _world = world;
-        _clearColor = Color(240, 240, 240);
+        // _clearColor = Color(240, 240, 240);
+        _clearColor = Color(20, 20, 20);
     }
 
     /// load resources
     void load() {
-        // auto image = raylib.LoadImage(Resources.path(R_FONT));
-        auto tex2 = raylib.LoadTexture(Resources.path(R_FONT));
+        _font = TermFont(
+            raylib.LoadTexture(Resources.path(R_FONT)),
+            16, 12
+        );
     }
 
     void render() {
         raylib.ClearBackground(_clearColor);
+        drawChar('e', Vector2(100, 100));
+        drawText("hello, world!", Vector2(100, 120));
+    }
+
+    void drawChar(char charId, Vector2 pos) {
+        auto fontX = charId % _font.gridSize;
+        auto fontY = charId / _font.gridSize;
+        raylib.DrawTextureRec(_font.texture,
+            Rectangle(fontX * _font.charSize, fontY * _font.charSize, _font.charSize, _font.charSize),
+            pos, WHITE);
+    }
+
+    void drawText(string text, Vector2 pos, float spacing = 1) {
+        Vector2 offset = Vector2(0, 0);
+        for (int i = 0; i < text.length; i++) {
+            drawChar(text[i], Vector2Add(pos, offset));
+            offset = Vector2Add(offset, Vector2(cast(int) (_font.charSize * spacing), 0));
+        }
     }
 }
