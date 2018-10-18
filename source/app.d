b6d92be109257fdb9c3cfd52acebec8e94d244b9
0xFireball <0xFireball@outlook.com>
2018-10-17 18:47:45 -0700

    create project

---
+++ source/app.d
@@ -0,0 +1,9 @@
+module guas.app;
+
+import std.stdio;
+
+import guas.meta;
+
+void main() {
+	writefln("guas [engine] v%s", VERSION);
+}
---
+++ source/meta.d
@@ -0,0 +1,4 @@
+module guas.meta;
+
+/// guas version
+enum VERSION = "0.0.1-dev";
