d24bad4b3396e5feaefc784f6a770bad50ed986b
0xFireball <0xFireball@outlook.com>
2018-10-18 17:27:07 -0700

    add basic map drawer

--- source/samp/demorogterm.d
+++ source/samp/demorogterm.d
@@ -10,68 +10,103 @@ import guas.graphics.terminal;
 import guas.graphics.frame;
 
 class DemoRogTerm : Terminal {
+    int _counter = 0;
+    Point mapSize;
+    int[] map;
+    Point playerPos;
+
     this(Renderer renderer) {
         super(renderer, Point(90, 46));
+        mapSize = Point(20, 20);
+        map = new int[mapSize.x * mapSize.y];
+        map = [
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1,
+            0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1,
+            0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1,
+            0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1,
+            0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1,
+            0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1,
+            0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1,
+            0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1,
+            0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1,
+            0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1,
+            0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+        ];
+        playerPos = Point(7, 8);
     }
 
     override void update() {
         if (_renderer._frame == 0) {
-            Frame sidebar = new Frame(this, Rect(74, 0, 16, this._dimens.y));
-            sidebar.outline(GRAY);
-            sidebar.fill(GRAY);
-            this.addFrame(sidebar);
-
-            this.setCursor(Point(sidebar.bounds.x, sidebar.bounds.y));
-            this.setColor(GREEN);
-            this.setBg(BLUE);
-            this.print("fake rogue.\n");
-            this.setBg(GRAY);
-            this._cur.x = 74;
-            this.print("HP: [14 / 15]\n");
-            this._cur.x = 74;
-            this.print("STR: 8\n");
-            this._cur.x = 74;
-            this.print("INT: 12\n");
-            this._cur.x = 74;
-            this.print("PER: 10\n");
-            this._cur.x = 74;
-            this.print("DEX: 9\n");
-
-            // draw screen
-            Frame field = new Frame(this, Rect(0, 0, 74, this._dimens.y));
-            field.outline(GREEN);
-            field.fill(BLACK);
-            this.addFrame(field);
-            // print field
-            this.setCursor(Point(0, 0));
-            this.setColor(GREEN);
-            this.setBg(BLACK);
-
-            this.print(
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "...................Û..................................................\n" ~
-                "..................ÛÛ......@...........................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n" ~
-                "......................................................................\n"
-            );
-
+            drawSidebar();
+            drawField();
         }
+
+        // print field
+        this.setCursor(Point(0, 0));
+
+        // draw the field
+        for (int y = 0; y < mapSize.y; y++) {
+            for (int x = 0; x < mapSize.x; x++) {
+                this.setColor(GREEN);
+                this.setBg(BLACK);
+                int dch = map[y * mapSize.x + x];
+                switch (dch) {
+                    case 0:
+                        this.print(".");
+                        break;
+                    case 1:
+                        this.print("\xDB");
+                        break;
+                    default:
+                        this.print("?");
+                        break;
+                }
+            }
+            print("\n");
+        }
+
+        // draw the player
+        this.setCursor(playerPos);
+        this.print("@");
+    }
+
+    private void drawSidebar() {
+        Frame sidebar = new Frame(this, Rect(74, 0, 16, this._dimens.y));
+        sidebar.outline(GRAY);
+        sidebar.fill(GRAY);
+        this.addFrame(sidebar);
+
+        this.setCursor(Point(sidebar.bounds.x, sidebar.bounds.y));
+        this.setColor(GREEN);
+        this.setBg(BLUE);
+        this.print("fake rogue.\xDB\n");
+        this.setBg(GRAY);
+        this._cur.x = 74;
+        this.print("HP: [14 / 15]\n");
+        this._cur.x = 74;
+        this.print("STR: 8\n");
+        this._cur.x = 74;
+        this.print("INT: 12\n");
+        this._cur.x = 74;
+        this.print("PER: 10\n");
+        this._cur.x = 74;
+        this.print("DEX: 9\n");
+    }
+
+    private void drawField() {
+        // draw screen
+        Frame field = new Frame(this, Rect(0, 0, 74, this._dimens.y));
+        field.outline(GREEN);
+        field.fill(BLACK);
+        this.addFrame(field);
     }
 }
